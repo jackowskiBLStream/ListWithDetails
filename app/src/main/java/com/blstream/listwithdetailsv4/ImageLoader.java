f8c7c1b3bfb8bc83b5fe41fdc566987fd38b2f63
Patryk Gwiazdowski <pgwiazdowski@gmail.com>
2016-03-11 11:47:28 +0100

    added ImageLoader class whih is teoreticcally working

--- app/src/main/java/com/blstream/listwithdetailsv4/ImageLoader.java
+++ app/src/main/java/com/blstream/listwithdetailsv4/ImageLoader.java
@@ -27,7 +27,8 @@ class ImageLoader extends AsyncTask<Bundle, Void, Bitmap> {
     @Override
     protected Bitmap doInBackground(Bundle... params) {
         bundle = params[0];
-        return decodeSampledBitmapFromResource((Resources)bundle.get("resources"), data, 100, 100);
+        //return decodeSampledBitmapFromResource((Resources)bundle.get("resources"), data, 100, 100);
+        return null;
     }
 
     // Once complete, see if ImageView is still around and set bitmap.
@@ -41,42 +42,4 @@ class ImageLoader extends AsyncTask<Bundle, Void, Bitmap> {
         }
     }
 
-    private Bitmap decodeSampledBitmapFromResource(Resources res, int resId,
-                                                   int reqWidth, int reqHeight) {
-
-        // First decode with inJustDecodeBounds=true to check dimensions
-        final BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inJustDecodeBounds = true;
-        BitmapFactory.decodeResource(res, resId, options);
-
-        // Calculate inSampleSize
-        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
-
-        // Decode bitmap with inSampleSize set
-        options.inJustDecodeBounds = false;
-        return BitmapFactory.decodeResource(res, resId, options);
-    }
-
-    private int calculateInSampleSize(
-            BitmapFactory.Options options, int reqWidth, int reqHeight) {
-        // Raw height and width of image
-        final int height = options.outHeight;
-        final int width = options.outWidth;
-        int inSampleSize = 1;
-
-        if (height > reqHeight || width > reqWidth) {
-
-            final int halfHeight = height / 2;
-            final int halfWidth = width / 2;
-
-            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
-            // height and width larger than the requested height and width.
-            while ((halfHeight / inSampleSize) > reqHeight
-                    && (halfWidth / inSampleSize) > reqWidth) {
-                inSampleSize *= 2;
-            }
-        }
-
-        return inSampleSize;
-    }
 }
